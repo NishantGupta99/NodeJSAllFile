decda46e328b6afcd93e11f3fe30687b633b7b0b
nishant Gupta <ngactive99@gmail.com>
2021-07-14 18:41:45 +0530

    i have created multiple files linked to node js

---
+++ OsModule.js
@@ -0,0 +1,4 @@
+const os = require('os');
+console.log(os.freemem()); // tells you the free memory in your computer    
+console.log(os.homedir()); // tells you your home directory 
+console.log(os.hostname()) ; // tells you the host name of your computer 
