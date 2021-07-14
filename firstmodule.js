715b04d9baa0b19aae1e6a31841f2b2d0e9235d6
nishant Gupta <ngactive99@gmail.com>
2021-07-14 19:51:45 +0530

    compleated with modules of cjs and ecma

--- firstmodule.js
+++ firstmodule.js
@@ -5,5 +5,8 @@ addTwo(); */// calling the function we created in the other second module
 // now we will be working on es6 module to that we have to change the extention yo mjs from js as the first step itself
 //import  {addTwo}  from "./secondmodule.mjs"; // this is the normal way of importing in esma script 
 //addTwo()
-import  {addTwo as addNum}  from "./secondmodule.mjs" // this is used to store call a function as another functions 
+//import  {addTwo as addNum}  from "./secondmodule.mjs" // this is used to store call a function as another functions 
 addNum();
+//import  {addTwo, addNum}  from "./secondmodule.mjs" // to call multiple function at once 
+import  * as add  from "./secondmodule.mjs" //this is used to import all the functions from the export group and store it in the function add
+console.log(add());
--- secondmodule.mjs
+++ secondmodule.mjs
@@ -1,7 +1,7 @@
 export function addTwo() {
     console.log(2+2);
   }; // we created a javascript function 
-  export function addNum() {
+  export default function addNum() {
     console.log(85+65+96);
   };
  //module.exports = addTwo; // this is how we export using common javascript 
