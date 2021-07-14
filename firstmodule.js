1712a4fd79d527374e0b0c09a9846746b7dc8d64
nishant Gupta <ngactive99@gmail.com>
2021-07-14 19:41:39 +0530

    import and export function and its types

--- firstmodule.js
+++ firstmodule.js
@@ -1,5 +1,9 @@
-const addTwo = require("./secondmodule"); // this is the format we use to import in common javasccript 
+/*const addTwo = require("./secondmodule"); // this is the format we use to import in common javasccript 
 
-addTwo(); // calling the function we created in the other second module
+addTwo(); */// calling the function we created in the other second module
 
 // now we will be working on es6 module to that we have to change the extention yo mjs from js as the first step itself
+//import  {addTwo}  from "./secondmodule.mjs"; // this is the normal way of importing in esma script 
+//addTwo()
+import  {addTwo as addNum}  from "./secondmodule.mjs" // this is used to store call a function as another functions 
+addNum();
--- secondmodule.mjs
+++ secondmodule.mjs
@@ -1,7 +1,9 @@
-function addTwo() {
+export function addTwo() {
     console.log(2+2);
-  } // we created a javascript function 
-
-  module.exports = addTwo; // this is how we export using common javascript 
+  }; // we created a javascript function 
+  export function addNum() {
+    console.log(85+65+96);
+  };
+ //module.exports = addTwo; // this is how we export using common javascript 
 
   
