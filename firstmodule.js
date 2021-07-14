eafffac1176cd76b43013bc810819e89236db495
nishant Gupta <ngactive99@gmail.com>
2021-07-14 19:19:01 +0530

    learning about the difference between common javascript and ecmma

---
+++ firstmodule.js
@@ -0,0 +1,5 @@
+const addTwo = require("./secondmodule"); // this is the format we use to import in common javasccript 
+
+addTwo(); // calling the function we created in the other second module
+
+// now we will be working on es6 module to that we have to change the extention yo mjs from js as the first step itself
---
+++ secondmodule.mjs
@@ -0,0 +1,7 @@
+function addTwo() {
+    console.log(2+2);
+  } // we created a javascript function 
+
+  module.exports = addTwo; // this is how we export using common javascript 
+
+  
