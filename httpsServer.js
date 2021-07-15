68bee3ca8915f7d521cbffd72f27433ae03de173
nishant Gupta <ngactive99@gmail.com>
2021-07-15 18:10:04 +0530

    starting to working a live server with node js

--- httpsServer.js
+++ httpsServer.js
@@ -4,7 +4,7 @@ const http = require("http");
 const port = process.env.PORT || 3000; // we are creating the port variable and storing the port value to be 3000
 
 const server = http.createServer((req, res) => {
-    console.log(req); // we can use this to print request type in the console window 
+    console.log(req.url); // we can use this to print request type in the console window //add . url when you want to know the url entered in chrome 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
     res.end('<h1> This is Nishant Gupta </h1> <p> we are learning https server <p>');  
