c27ccbc13eaafa82483429cb1b4e108598727ef5
nishant Gupta <ngactive99@gmail.com>
2021-07-15 16:22:21 +0530

    console.log(req) added

--- httpsServer.js
+++ httpsServer.js
@@ -1,9 +1,10 @@
-const { Console } = require("console");
+
 const http = require("http");
 
 const port = process.env.PORT || 3000; // we are creating the port variable and storing the port value to be 3000
 
 const server = http.createServer((req, res) => {
+    console.log(req); // we can use this to print request type in the console window 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
     res.end('<h1> This is Nishant Gupta </h1> <p> we are learning https server <p>');  
